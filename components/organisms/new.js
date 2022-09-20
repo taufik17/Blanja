ed878b003130f8293a8ab6015816f497a5d24dfd
taufik17 <taufikagungsantoso17@gmail.com>
2022-09-20 17:46:12 +0700

    [add] news

--- components/organisms/new.js
+++ components/organisms/new.js
@@ -35,6 +35,136 @@ function New() {
             </div>
           </div>
         </div>
+        <div className="col-lg-3 col-md-6">
+          <div className="card">
+            <Image
+              src="/image/new.jpg"
+              alt="Logo"
+              width={350}
+              height={210}
+              className="card-img-top"
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Men{"'"}s formal suit - Black & White
+              </h5>
+              <div className="card-text">
+                <h5 className={StyleLogin.textRed}>$ 40.0</h5>
+                <small>Zalora Cloth</small>
+              </div>
+              <Image
+                src="/image/5star.svg"
+                alt="Logo"
+                width={120}
+                height={20}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-6">
+          <div className="card">
+            <Image
+              src="/image/new.jpg"
+              alt="Logo"
+              width={350}
+              height={210}
+              className="card-img-top"
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Men{"'"}s formal suit - Black & White
+              </h5>
+              <div className="card-text">
+                <h5 className={StyleLogin.textRed}>$ 40.0</h5>
+                <small>Zalora Cloth</small>
+              </div>
+              <Image
+                src="/image/5star.svg"
+                alt="Logo"
+                width={120}
+                height={20}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-6">
+          <div className="card">
+            <Image
+              src="/image/new.jpg"
+              alt="Logo"
+              width={350}
+              height={210}
+              className="card-img-top"
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Men{"'"}s formal suit - Black & White
+              </h5>
+              <div className="card-text">
+                <h5 className={StyleLogin.textRed}>$ 40.0</h5>
+                <small>Zalora Cloth</small>
+              </div>
+              <Image
+                src="/image/5star.svg"
+                alt="Logo"
+                width={120}
+                height={20}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-6">
+          <div className="card">
+            <Image
+              src="/image/new.jpg"
+              alt="Logo"
+              width={350}
+              height={210}
+              className="card-img-top"
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Men{"'"}s formal suit - Black & White
+              </h5>
+              <div className="card-text">
+                <h5 className={StyleLogin.textRed}>$ 40.0</h5>
+                <small>Zalora Cloth</small>
+              </div>
+              <Image
+                src="/image/5star.svg"
+                alt="Logo"
+                width={120}
+                height={20}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-6">
+          <div className="card">
+            <Image
+              src="/image/new.jpg"
+              alt="Logo"
+              width={350}
+              height={210}
+              className="card-img-top"
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Men{"'"}s formal suit - Black & White
+              </h5>
+              <div className="card-text">
+                <h5 className={StyleLogin.textRed}>$ 40.0</h5>
+                <small>Zalora Cloth</small>
+              </div>
+              <Image
+                src="/image/5star.svg"
+                alt="Logo"
+                width={120}
+                height={20}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
