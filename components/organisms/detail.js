f08c936e2b220f8c20878ac1d6a43d69b3f68f99
taufik17 <taufikagungsantoso17@gmail.com>
2022-09-25 08:43:00 +0700

    [fix] rating and review

--- components/organisms/detail.js
+++ components/organisms/detail.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
-import { Breadcrumb } from "react-bootstrap";
+import { Breadcrumb, Button } from "react-bootstrap";
 import HomeStyle from "../../styles/Home.module.css";
 import { FiPlus, FiMinus } from "react-icons/fi";
 import StyleMyBag from "../../styles/mybag.module.css";
+import StyleLogin from "../../styles/login.module.css";
 
 function Detail() {
   const [black, setblack] = useState(false);
@@ -127,7 +128,7 @@ function Detail() {
                 <FiPlus className={StyleMyBag.iconPlus} />
               </div>
             </div>
-            
+
             <div className="col-2 offset-2">
               <b>Jumlah</b>
               <div className="mt-2 d-flex justify-content-between">
@@ -137,8 +138,152 @@ function Detail() {
               </div>
             </div>
           </div>
+          <div className="mt-3 row">
+            <div className="col-3">
+              <div className="d-grid gap-2 my-2">
+                <Button className={`${StyleLogin.btnRegister} px-5`}>
+                  Chat
+                </Button>
+              </div>
+            </div>
+            <div className="col-3">
+              <div className="d-grid gap-2 my-2">
+                <Button className={`${StyleLogin.btnRegister} px-5`}>
+                  Add bag
+                </Button>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="d-grid gap-2 my-2">
+                <Button className={`${StyleLogin.btnLogin} mx-2 px-4`}>
+                  Buy Now
+                </Button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+
+      <h5>Informasi Produk</h5>
+      <h6>Condition</h6>
+      <b className={StyleLogin.textRed}>New</b>
+      <h5>Description</h5>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+      <p>
+        Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus
+        quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames
+        ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit
+        imperdiet ac ac felis. Etiam tincidunt tristique placerat. Pellentesque
+        a consequat mauris, vel suscipit ipsum. Donec ac mauris vitae diam
+        commodo vehicula. Donec quam elit, sollicitudin eu nisl at, ornare
+        suscipit magna.
+      </p>
+
+      <p>
+        Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus
+        quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames
+        ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit
+        imperdiet ac ac felis.
+      </p>
+
+      <p>In ultricies rutrum tempus. Mauris vel molestie orci.</p>
+
+      <h5 className="mt-3">Product review</h5>
+      <div className="row mt-3 mb-5">
+        <div className="col-2">
+          <div className="d-flex">
+            <h1 className={StyleMyBag.rating}>5.0</h1>
+            <p className="pt-4">/10</p>
+          </div>
+          <div className="d-flex">
+            <Image src="/image/Star.svg" alt="Logo" width={20} height={20} />
+            <Image src="/image/Star.svg" alt="Logo" width={20} height={20} />
+            <Image src="/image/Star.svg" alt="Logo" width={20} height={20} />
+            <Image src="/image/Star.svg" alt="Logo" width={20} height={20} />
+            <Image src="/image/Star.svg" alt="Logo" width={20} height={20} />
+          </div>
+        </div>
+        <div className="col-4">
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <Image
+                    src="/image/Star.svg"
+                    alt="Logo"
+                    width={15}
+                    height={15}
+                  />
+                </td>
+                <td>5</td>
+                <td className="px-3">
+                  <Image
+                    src="/image/line.svg"
+                    alt="Logo"
+                    width={150}
+                    height={15}
+                  />
+                </td>
+                <td>4</td>
+              </tr>
+              <tr>
+                <td>
+                  <Image
+                    src="/image/Star.svg"
+                    alt="Logo"
+                    width={15}
+                    height={15}
+                  />
+                </td>
+                <td>4</td>
+                <td></td>
+                <td>0</td>
+              </tr>
+              <tr>
+                <td>
+                  <Image
+                    src="/image/Star.svg"
+                    alt="Logo"
+                    width={15}
+                    height={15}
+                  />
+                </td>
+                <td>3</td>
+                <td></td>
+                <td>0</td>
+              </tr>
+              <tr>
+                <td>
+                  <Image
+                    src="/image/Star.svg"
+                    alt="Logo"
+                    width={15}
+                    height={15}
+                  />
+                </td>
+                <td>2</td>
+                <td></td>
+                <td>0</td>
+              </tr>
+              <tr>
+                <td>
+                  <Image
+                    src="/image/Star.svg"
+                    alt="Logo"
+                    width={15}
+                    height={15}
+                  />
+                </td>
+                <td>1</td>
+                <td></td>
+                <td>0</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <hr />
     </>
   );
 }
